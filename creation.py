5812da53f0d5e73987b9c1afed3004554d6b2c5f
Ahmed-One <107000779+Ahmed-One@users.noreply.github.com>
2024-12-11 11:19:27 +0200

    First Commit

---
+++ creation.py
@@ -0,0 +1,67 @@
+# Create the coefficient matrices
+import numpy as np
+
+# Simple problem
+# 2 local nodes, 2 clouds, 4-function workflows
+
+# Functions
+# 0: start, 1: inits, 2: train, 3: load, 4:end
+names_funs = ['start', 'inits', 'train', 'load', 'end']
+start_ends = [0, 4]
+funs_cloud = [2]
+times = [0, 2, 8, 3, 0]
+rams = [0, 3, 12, 2, 0]
+num_funs = len(times)
+
+# TinyFaaS nodes (2-nodes/workflows)
+names_tiny = ['0', '1']
+rams_tiny = [5, 3]
+num_tiny = len(names_tiny)
+
+# Clouds (2-providers)
+names_cloud = ['G', 'A']
+prices_cloud_transfer = [10, 12]
+prices_cloud_ram = [12, 10]
+prices_cloud_start = [20, 22]
+num_clouds = len(names_cloud)
+
+# some helpful data
+num_nodes = num_tiny + num_clouds
+prices_transfer = np.append(np.zeros(num_tiny), prices_cloud_transfer)
+prices_ram = np.append(np.zeros(num_tiny), prices_cloud_ram)
+prices_start = np.append(np.zeros(num_tiny), prices_cloud_start)
+ram_limits = np.append(np.array(rams_tiny), np.inf * np.ones(num_clouds))
+
+# Latency
+# TinyFaaS: 0, 1, Cloud: 2, 3
+latency = [[0, 0.01, 0.1, 0.2],
+           [0.01, 0, 0.2, 0.1],
+           [0.1, 0.2, 0, 0.3],
+           [0.2, 0.1, 0.3, 0]]
+
+# Coefficient matrices
+# Latency between nodes
+L = np.array(latency)  # L_j,i
+
+# Time for running a function on a node
+T = np.zeros((num_tiny, num_funs, num_nodes))  # T_n,m,j
+for workflow in range(num_tiny):
+    for node in range(num_nodes):
+        T[workflow, :, node] = np.array(times)
+
+# Cost of transferring data
+D = np.zeros((num_tiny, num_funs, num_nodes, num_nodes))  # D_n,m,j,i
+for workflow in range(num_tiny):
+    for function in range(num_funs):
+        for node_sending in range(num_nodes):
+            for node_receiving in range(num_nodes):
+                D[workflow, function, node_sending, node_receiving] = rams[function] * prices_ram[node_sending] + \
+                                                                      rams[function] * prices_ram[node_receiving]
+# Cost of running a function
+C = np.zeros((num_tiny, num_funs, num_nodes))  # C_n,m,j
+for workflow in range(num_tiny):
+    for function in range(num_funs):
+        for node in range(num_nodes):
+            C[workflow, function, node] = prices_start[node] + rams[function] * prices_ram[node]
+
+pass
---
+++ optimization.py
@@ -0,0 +1,73 @@
+from gurobipy import *
+from creation import *
+
+model = Model("TinyFaaS")
+P = model.addVars(num_tiny, num_funs, num_nodes, vtype=GRB.BINARY, name="P")  # P_n,m,i
+
+# expressions for latency, and data transfer costs
+obj_latency = 0
+obj_transfer = 0
+for workflow in range(num_tiny):
+    for function in range(num_funs - 1):
+        for node_sending in range(num_nodes):
+            for node_receiving in range(num_nodes):
+                obj_latency = obj_latency + P[workflow, function, node_sending] \
+                              * P[workflow, function + 1, node_receiving] \
+                              * L[node_sending, node_receiving]
+                obj_transfer = obj_transfer + P[workflow, function, node_sending] \
+                               * P[workflow, function + 1, node_receiving] \
+                               * D[workflow, function, node_sending, node_receiving]
+
+# expressions for time, and data running costs
+obj_time = 0
+obj_ram = 0
+for workflow in range(num_tiny):
+    for function in range(num_funs):
+        for node in range(num_nodes):
+            obj_time = obj_time + P[workflow, function, node] * T[workflow, function, node]
+            obj_ram = obj_ram + P[workflow, function, node] * C[workflow, function, node]
+
+# expression for the total objective function
+w_1, w_2 = 0.5, 0.3  # w1 for latency and time, w2 for costs
+obj = w_1 * (obj_latency + obj_time) + w_2 * (obj_transfer + obj_ram)
+
+## Constraints
+
+# start and end functions should be assigned to TinyFaaS nodes
+# P_n,(0,5,7),n = 1 for all n else 0
+for workflow in range(num_tiny):
+    for node in range(num_nodes):
+        for function in start_ends:
+            if node < num_tiny:
+                # model.addConstr(P[workflow, function, node] - 1 == 0)
+                pass
+            else:
+                model.addConstr(P[workflow, function, node] == 0)
+
+# training functions can not be executed in local nodes
+# sum_k P_n,4,k = 0 , k in {0 .. 3}
+for workflow in range(num_tiny):
+    for function in funs_cloud:
+        model.addConstr(quicksum(P[workflow, function, node] for node in range(num_tiny)) == 0)
+
+# maximum ram limit per sum of functions m on node k
+# sum_n sum_k P_n,m,k * RAM_n,m <= MAX_k , for all k in {0 ..3} for all n, m
+for workflow in range(num_tiny):
+    for node in range(num_nodes):
+        model.addConstr(quicksum(P[workflow, function, node] * rams[function]
+                                 for function in range(num_funs)) <= ram_limits[node])
+
+# each function can only be assigned to a single node
+for workflow in range(num_tiny):
+    for node in range(num_nodes):
+        model.addConstr(quicksum(P[workflow, function, node] for function in range(num_funs)) == 1)
+
+# Solve model
+model.setObjective(obj, GRB.MINIMIZE)
+model.setParam("OutputFlag", 1)
+model.update()
+model.optimize()
+
+# Print result
+P_out = np.reshape(np.array([item.x for item in model.getVars()]), (num_tiny, num_funs, num_nodes))
+print(P_out)
