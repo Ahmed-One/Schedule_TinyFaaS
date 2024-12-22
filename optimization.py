ee889f6412cd4ae145f05648711044db77dc28c8
Ahmed-One <107000779+Ahmed-One@users.noreply.github.com>
2024-12-22 18:03:00 +0200

    Add objectives as properties of the optimizer class

--- optimization.py
+++ optimization.py
@@ -9,32 +9,32 @@ class Optimizer:
                                     vtype=GRB.INTEGER, name="P")  # P_n,m,i
 
         # expressions for L, and data transfer costs
-        obj_latency = 0
-        obj_transfer = 0
+        self.obj_latency = 0
+        self.obj_transfer = 0
         for workflow in range(wf.num_workflows):
             for function in range(wf.num_funs - 1):
                 for node_sending in range(pb.num_nodes):
                     for node_receiving in range(pb.num_nodes):
-                        obj_latency = obj_latency + self.P[workflow, function, node_sending] \
-                                      * self.P[workflow, function + 1, node_receiving] \
-                                      * pb.L[node_sending, node_receiving]
-                        obj_transfer = obj_transfer + self.P[workflow, function, node_sending]\
-                                       * self.P[workflow, function + 1, node_receiving]\
-                                       * pb.D[workflow, function, node_sending, node_receiving]
+                        self.obj_latency = self.obj_latency + self.P[workflow, function, node_sending] \
+                                           * self.P[workflow, function + 1, node_receiving] \
+                                           * pb.L[node_sending, node_receiving]
+                        self.obj_transfer = self.obj_transfer + self.P[workflow, function, node_sending] \
+                                            * self.P[workflow, function + 1, node_receiving] \
+                                            * pb.D[workflow, function, node_sending, node_receiving]
 
         # expressions for time, and data running costs
-        obj_time = 0
-        obj_ram = 0
+        self.obj_time = 0
+        self.obj_ram = 0
         for workflow in range(wf.num_workflows):
             for function in range(wf.num_funs):
                 for node in range(pb.num_nodes):
-                    obj_time = obj_time + self.P[workflow, function, node] * pb.T[workflow, function, node]
-                    obj_ram = obj_ram + self.P[workflow, function, node] * pb.C[workflow, function, node]
+                    self.obj_time = self.obj_time + self.P[workflow, function, node] * pb.T[workflow, function, node]
+                    self.obj_ram = self.obj_ram + self.P[workflow, function, node] * pb.C[workflow, function, node]
 
         # expression for the total objective function
         self.w_1, self.w_2 = 1, 470000  # w1 for L and time, w2 for costs
-        obj = self.w_1 * (obj_latency + obj_time) + self.w_2 * (obj_transfer + obj_ram)
-        self.model.setObjective(obj, GRB.MINIMIZE)
+        self.obj = self.w_1 * (self.obj_latency + self.obj_time) + self.w_2 * (self.obj_transfer + self.obj_ram)
+        self.model.setObjective(self.obj, GRB.MINIMIZE)
 
         # Constraints
 
@@ -64,7 +64,6 @@ class Optimizer:
             for function in range(wf.num_funs):
                 self.model.addConstr(quicksum(self.P[workflow, function, node] for node in range(pb.num_nodes))
                                      == wf.funs_counts[workflow][function])
-
 
     # Solve model
     def solve(self):
