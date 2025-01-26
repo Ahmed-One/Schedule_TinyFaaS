5b36905d43bcc43389788ed8b84681e20736ca3a
Ahmed-One <107000779+Ahmed-One@users.noreply.github.com>
2025-01-26 09:49:25 +0200

    New formulation as optimization of edges rather than deployment

--- optimization.py
+++ optimization.py
@@ -3,8 +3,7 @@ from creation import *
 
 
 class Optimizer:
-    def __init__(
-            self, wf: Workflows, net: LocalNetwork, pb: Problem):
+    def __init__(self, wf: Workflows, net: LocalNetwork, pb: Problem):
         self.model = Model("TinyFaaS-Schedule")
         self.P = self.model.addVars(wf.num_workflows, wf.num_funs, pb.num_nodes,
                                     vtype=GRB.INTEGER, name="P")  # P_n,m,i
@@ -86,7 +85,7 @@ class Optimizer:
                 self.model.addConstr(quicksum(self.P[workflow, function, node] for node in range(net.num_tiny)) == 0)
 
         # maximum ram limit per sum of functions m on node k
-        # sum_n sum_k P_n,m,k * RAM_n,m <= MAX_k , for all k in {0 ..3} for all n, m
+        # sum_n sum_m P_n,m,k * RAM_n,m <= MAX_k , for all k in {0 ..3} for all n, m
         for node in range(pb.num_nodes):
             self.model.addConstr(
                 quicksum(quicksum(self.P[workflow, function, node] * wf.funs_data[workflow][function]
@@ -99,6 +98,128 @@ class Optimizer:
             for function in range(wf.num_funs):
                 self.model.addConstr(quicksum(self.P[workflow, function, node] for node in range(pb.num_nodes))
                                      == wf.funs_counts[workflow][function])
+
+    # Solve model
+    def solve(self):
+        self.model.setParam("OutputFlag", 1)
+        self.model.update()
+        self.model.optimize()
+
+
+class Optimizer2:
+    def __init__(self, wf: Workflows, net: LocalNetwork, pb: Problem):
+        self.model = Model("TinyFaaS-Schedule")
+        self.P = self.model.addVars(wf.num_workflows, wf.num_funs, pb.num_nodes, pb.num_nodes,
+                                    vtype=GRB.INTEGER, name="P")  # P_n,m,i,j
+
+        # expressions for L, and data transfer costs
+        self.obj_latency = 0
+        self.obj_transfer = 0
+        self.obj_latency_workflows = []
+        self.obj_transfer_workflows = []
+        for workflow in range(wf.num_workflows):
+            obj_latency_functions = []
+            obj_transfer_functions = []
+            for function in range(wf.num_funs):
+                obj_latency_nodes = []
+                obj_transfer_nodes = []
+                for node_sending in range(pb.num_nodes):
+                    obj_latency = 0
+                    obj_transfer = 0
+                    for node_receiving in range(pb.num_nodes):
+                        obj_latency = obj_latency + self.P[workflow, function, node_sending, node_receiving] \
+                                      * pb.L[node_sending, node_receiving]
+                        self.obj_latency += obj_latency
+
+                        obj_transfer = obj_transfer + self.P[workflow, function, node_sending, node_receiving] \
+                                       * pb.D[workflow, function, node_sending, node_receiving]
+                        self.obj_transfer += obj_transfer
+
+                    obj_latency_nodes.append(obj_latency)
+                    obj_transfer_nodes.append(obj_transfer)
+                obj_latency_functions.append(obj_latency_nodes)
+                obj_transfer_functions.append(obj_transfer_nodes)
+            self.obj_latency_workflows.append(obj_latency_functions)
+            self.obj_transfer_workflows.append(obj_transfer_functions)
+
+        # expressions for time, and data running costs
+        self.obj_time = 0
+        self.obj_ram = 0
+        self.obj_time_workflows = []
+        self.obj_ram_workflows = []
+        for workflow in range(wf.num_workflows):
+            obj_time_functions = []
+            obj_ram_functions = []
+            for function in range(wf.num_funs):
+                obj_time_nodes = []
+                obj_ram_nodes = []
+                for node_sending in range(pb.num_nodes):
+                    for node_receiving in range(pb.num_nodes):
+                        obj_time = self.P[workflow, function, node_sending, node_receiving] \
+                                   * pb.T[workflow, function, node_sending]
+                        self.obj_time += obj_time
+
+                        obj_ram = self.P[workflow, function, node_sending, node_receiving] \
+                                  * pb.C[workflow, function, node_sending]
+                        self.obj_ram += obj_ram
+
+                        obj_time_nodes.append(obj_time)
+                        obj_ram_nodes.append(obj_ram)
+                obj_time_functions.append(obj_time_nodes)
+                obj_ram_functions.append(obj_ram_nodes)
+            self.obj_time_workflows.append(obj_time_functions)
+            self.obj_ram_workflows.append(obj_ram_functions)
+
+        # expression for the total objective function
+        self.w_1, self.w_2 = 1, 470000  # w1 for L and time, w2 for costs
+        self.obj = self.w_1 * (self.obj_latency + self.obj_time) + self.w_2 * (self.obj_transfer + self.obj_ram)
+        self.model.setObjective(self.obj, GRB.MINIMIZE)
+
+        # Constraints
+
+        # start and end functions should be assigned to TinyFaaS nodes
+        # P_n,(0,5,7),n,j = 1 for all n else 0
+        for workflow in range(wf.num_workflows):
+            for function in wf.funs_local[workflow]:
+                self.model.addConstr(quicksum(self.P[workflow, function, workflow, node_receiving]
+                                              for node_receiving in range(pb.num_nodes)) - 1 == 0)
+
+        # training functions can not be executed in local nodes
+        # sum_k P_n,4,k,j = 0 , k in {0 .. 3} and all j
+        for workflow in range(wf.num_workflows):
+            for function in wf.funs_cloud[workflow]:
+                self.model.addConstr(quicksum(quicksum(self.P[workflow, function, node_sending, node_receiving]
+                                                       for node_sending in range(net.num_tiny))
+                                              for node_receiving in range(pb.num_nodes)) == 0)
+
+        # maximum ram limit per sum of functions m on node k
+        # sum_n sum_m sum_j P_n,m,i,j * RAM_n,m <= RAM_MAX_i , for all i
+        # TODO check constraint in original optimizer
+        for node_sending in range(pb.num_nodes):
+            self.model.addConstr(quicksum(quicksum(quicksum(
+                self.P[workflow, function, node_sending, node_receiving] * wf.funs_data[workflow][function]
+                                for node_receiving in range(pb.num_nodes))
+                                for function in range(wf.num_funs))
+                                for workflow in range(wf.num_workflows)) <= pb.ram_limits[node_sending])
+
+        # each function can only be assigned to a single node
+        # sum_j P_n,m,i,j = 1 for all n,m
+        for workflow in range(wf.num_workflows):
+            for function in range(wf.num_funs):
+                self.model.addConstr(quicksum(quicksum(self.P[workflow, function, node_sending, node_receiving]
+                                                       for node_receiving in range(pb.num_nodes))
+                                              for node_sending in range(pb.num_nodes))
+                                     == wf.funs_counts[workflow][function])
+
+        # Continuation Constraint?
+        # TODO: Check!!
+        for workflow in range(wf.num_workflows):
+            for function in range(wf.num_funs - 1):
+                for node_receiving in range(pb.num_nodes):
+                    self.model.addConstr(quicksum(self.P[workflow, function, node_sending, node_receiving]
+                                                  for node_sending in range(pb.num_nodes)) ==
+                                         quicksum(self.P[workflow, function + 1, node_receiving, node_receiving_next]
+                                                  for node_receiving_next in range(pb.num_nodes)))
 
     # Solve model
     def solve(self):
