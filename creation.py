5f5b1c71f31fd836bb4a54fd6f56a967542b4feb
Ahmed-One <107000779+Ahmed-One@users.noreply.github.com>
2025-02-23 12:40:06 +0200

    Update calculations of various parameters, added others.

--- creation.py
+++ creation.py
@@ -7,33 +7,71 @@ from parsing import *
 class Problem:
     def __init__(self, wf: Workflows, net: LocalNetwork, cld: CloudsInfo):
         # some helpful data
-        self.num_nodes = net.num_tiny + cld.num_clouds
-        self.prices_transfer = np.append(np.zeros(net.num_tiny), cld.prices_cloud_transfer)
-        self.prices_ram = np.append(np.zeros(net.num_tiny), cld.prices_cloud_ram)
-        self.prices_start = np.append(np.zeros(net.num_tiny), cld.prices_cloud_start)
-        self.ram_limits = np.append(np.array(net.rams_tiny), np.inf * np.ones(cld.num_clouds))
+        self.num_nodes = net.num + cld.num
+        # The upload/download directionality is switched. For a local node sending to the cloud, that means that the
+        # local node is uploading and the cloud is downloading, thereby applying cloud upload pricing, and vice, versa.
+        self.prices_transfer_up = np.append(np.zeros(net.num), cld.prices_transfer_down)
+        self.prices_transfer_down = np.append(np.zeros(net.num), cld.prices_transfer_up)
+        self.prices_ram = np.append(np.zeros(net.num), cld.prices_ram)
+        self.prices_start = np.append(np.zeros(net.num), cld.prices_start)
+        self.ram_limits = np.append(np.array(net.rams), np.inf * np.ones(cld.num))
+        self.p_factors = np.append(np.array(net.p_factors), cld.p_factors)
 
         # Coefficient matrices
-        # Latency between nodes ([second] = tiny-to-router[second] + router-to-cloud[second])
-        self.L = np.zeros((self.num_nodes, self.num_nodes))  # L_i,j
-        latency_list = np.append(net.latency_local * np.ones(net.num_tiny), np.array(cld.latency_cloud))
-        for node_sending in range(self.num_nodes):
-            for node_receiving in range(self.num_nodes):
-                if node_sending == node_receiving:
-                    self.L[node_sending, node_receiving] = 0
-                elif node_sending < net.num_tiny and node_receiving < net.num_tiny:
-                    self.L[node_sending, node_receiving] = net.latency_local
-                else:
-                    self.L[node_sending, node_receiving] = latency_list[node_sending] + latency_list[node_receiving]
 
-        # Time for running a function on a node (second)
-        self.T = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes))  # T_n,m,j
+        # Latency time between nodes ([second] = data[GB] / transmission rate[GB/s])
+        self.L = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes, self.num_nodes))  # L_n,m,i,j
         for workflow in range(wf.num_workflows):
-            for node in range(self.num_nodes):
-                self.T[workflow, :, node] = np.array(wf.funs_times[workflow])
+            for function in range(wf.num_funs):
+                for node_sending in range(self.num_nodes):
+                    for node_receiving in range(self.num_nodes):
+                        transmission_rate = 0
+                        if node_sending == node_receiving:
+                            transmission_rate = np.inf
+                        elif node_sending < net.num and node_receiving < net.num:
+                            transmission_rate = net.rate_down
+                        elif node_sending < net.num <= node_receiving:
+                            # tinyFaaS uploading to cloud
+                            transmission_rate = net.rate_up
+                        elif node_sending >= net.num > node_receiving:
+                            # tinyFaaS downloading from cloud
+                            transmission_rate = net.rate_down
+                        elif node_sending >= net.num and node_receiving >= net.num:
+                            # transfer between clouds
+                            transmission_rate = np.inf
+
+                        self.L[workflow, function, node_sending, node_receiving] = \
+                            wf.funs_data[workflow, function] / transmission_rate
+
+        # Specific Time for running a function instance on a node (second)
+        self.T = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes))  # T_p_n,m,i
+        for workflow in range(wf.num_workflows):
+            for function in range(wf.num_funs):
+                for node in range(self.num_nodes):
+                    self.T[workflow, function, node] = np.array(
+                        wf.funs_times[workflow, function] / (self.p_factors[node]) * wf.funs_counts[workflow, function])
+
+        # Specific Time for transmitting a function instance data between nodes (second)
+        self.T_r = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes, self.num_nodes))  # T_r_n,m,i,j
+        for workflow in range(wf.num_workflows):
+            for function in range(wf.num_funs):
+                for node_sending in range(self.num_nodes):
+                    for node_receiving in range(self.num_nodes):
+                        # if local node uploads to cloud
+                        if node_sending < net.num <= node_receiving:
+                            data_transmit_rate = net.rate_up
+                        # if local node downloads from cloud
+                        elif node_sending >= net.num > node_receiving:
+                            data_transmit_rate = net.rate_down
+                        # if local node sends to local node or cloud to cloud
+                        else:
+                            data_transmit_rate = np.inf
+
+                        self.T_r[workflow, function, node_sending, node_receiving] = wf.funs_data[workflow, function] \
+                                                                                     / data_transmit_rate
 
         # Cost of transferring data (cost[$] = sum{data[GB] * cost[$/GB]})
-        self.D = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes, self.num_nodes))  # D_n,m,j,i
+        self.D = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes, self.num_nodes))  # D_n,m,i,j
         for workflow in range(wf.num_workflows):
             for function in range(wf.num_funs):
                 for node_sending in range(self.num_nodes):
@@ -42,14 +80,21 @@ class Problem:
                             self.D[workflow, function, node_sending, node_receiving] = 0
                         else:
                             self.D[workflow, function, node_sending, node_receiving] = \
-                                    wf.funs_data[workflow][function] * self.prices_ram[node_sending] + \
-                                    wf.funs_data[workflow][function] * self.prices_ram[node_receiving]
+                                wf.funs_data[workflow, function] * self.prices_transfer_up[node_sending] \
+                                + wf.funs_data[workflow, function] * self.prices_transfer_down[node_receiving]
 
-        # Cost of running a function ($ = price_start[$/num] * fun_count + fun_size[GB] * price_ram[$/GB] * fun_time[s])
-        self.C = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes))  # C_n,m,j
+        # Cost of running a function ($ = price_ram[$/GB] * (fun_size[GB] + fun_data[GB]))
+        self.C = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes))  # C_n,m,i
         for workflow in range(wf.num_workflows):
             for function in range(wf.num_funs):
                 for node in range(self.num_nodes):
-                    self.C[workflow, function, node] = self.prices_start[node] * wf.funs_counts[workflow][function]\
-                                                       + wf.funs_sizes[workflow][function] * self.prices_ram[node]\
-                                                       * wf.funs_times[workflow][function]
+                    self.C[workflow, function, node] = self.prices_ram[node] * self.T[workflow, function][node] * \
+                                                       (wf.funs_sizes[workflow, function]
+                                                        + wf.funs_data[workflow, function])
+
+        # Cost of starting a function ($/start)
+        self.C_s = np.zeros((wf.num_workflows, wf.num_funs, self.num_nodes))  # C_s_n,m,i
+        for workflow in range(wf.num_workflows):
+            for function in range(wf.num_funs):
+                for node in range(self.num_nodes):
+                    self.C_s[workflow, function, node] = self.prices_start[node]
