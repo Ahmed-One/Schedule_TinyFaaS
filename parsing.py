4006002349a51a6c47c2b89f8e2a0d71e0b77dd1
Ahmed-One <107000779+Ahmed-One@users.noreply.github.com>
2025-02-23 12:37:49 +0200

    Add function persistence and others. Use dictionaries for better indexing.

--- parsing.py
+++ parsing.py
@@ -9,69 +9,57 @@ class Workflows:
             workflows = json.load(f)
 
         # Extract workflows and functions' properties
-        self.num_workflows = 0
-        self.names_workflows = []
-        self.names_funs = []
-        self.funs_local = []
-        self.funs_cloud = []
-        self.funs_times = []
-        self.funs_data = []
-        self.funs_sizes = []
-        self.funs_counts = []
+        self.num_workflows = len(workflows)
+        self.names_workflows = list(workflows.keys())
+        self.names_funs = {}
+        self.funs_times = {}
+        self.funs_data = {}
+        self.funs_sizes = {}
+        self.funs_counts = {}
+        self.funs_local = {}
+        self.funs_cloud = {}
+        self.funs_persistent = {}
         num_workflow_funs = []
 
-        for workflow in workflows:
-            self.names_workflows.append(workflow)
+        for wi, workflow in enumerate(workflows.values()):
+            self.names_funs[wi] = []
+            self.funs_local[wi] = []
+            self.funs_cloud[wi] = []
+            self.funs_persistent[wi] = []
 
-        for workflow in workflows.values():
-            self.num_workflows += 1
-            fun_count = []
-            names = []
-            times = []
-            datas = []
-            sizes = []
-            start_ends = []
-            clouds = []
-            index = 0
             # extract workflow function information
-            for index, function in enumerate(workflow["functions"]):
-                names.append(function)
+            for fi, function in enumerate(workflow["functions"]):
+                self.names_funs[wi].append(function)
                 fun_info = workflow["functions"][function]
-                times.append(fun_info["time"])  # second
-                datas.append(fun_info["data"])  # GB
-                sizes.append(fun_info["size"])  # GB
+                self.funs_times[wi, fi] = fun_info["time"]  # second
+                self.funs_data[wi, fi] = fun_info["data"]  # GB
+                self.funs_sizes[wi, fi] = fun_info["size"]  # GB
+                self.funs_counts[wi, fi] = fun_info["count"]  # number of function instances
                 if fun_info["location"] == "local":
-                    start_ends.append(index)
+                    self.funs_local[wi].append(fi)
                 elif fun_info["location"] == "cloud":
-                    clouds.append(index)
-                fun_count.append(fun_info["count"])  # test
+                    self.funs_cloud[wi].append(fi)
+                if fun_info["run"] == "always":
+                    self.funs_persistent[wi].append(fi)
 
             # store the number of functions in a workflow
-            num_workflow_funs.append(index + 1)
-
-            self.names_funs.append(names)
-            self.funs_times.append(times)
-            self.funs_data.append(datas)
-            self.funs_sizes.append(sizes)
-            self.funs_local.append(start_ends)
-            self.funs_cloud.append(clouds)
-            self.funs_counts.append(fun_count)
+            num_workflow_funs.append(len(workflow["functions"].keys()))
 
         # Pad workflows in case of varying number of functions
         # get the number of functions from the maximum length workflow
         self.num_funs = max(num_workflow_funs)
         # Check if not all workflows have same number of functions
         if len(set(num_workflow_funs)) != 1:
-            for index, workflow in enumerate(self.names_funs):
+            for wi, workflow in enumerate(self.names_funs.values()):
                 workflow_size = len(workflow)
                 if workflow_size < self.num_funs:
                     for gap in range(self.num_funs - workflow_size):
-                        self.names_funs[index].append("dummy")
-                        self.funs_times[index].append(0)
-                        self.funs_data[index].append(0)
-                        self.funs_sizes[index].append(0)
-                        self.funs_counts[index].append(1)
-                        self.funs_local[index].append(workflow_size + gap)
+                        self.names_funs[wi].append("dummy")
+                        self.funs_times[wi, gap].append(0)
+                        self.funs_data[wi, gap].append(0)
+                        self.funs_sizes[wi, gap].append(0)
+                        self.funs_counts[wi, gap].append(1)
+                        self.funs_local[wi].append(workflow_size + gap)
 
 
 class LocalNetwork:
@@ -82,10 +70,14 @@ class LocalNetwork:
 
         # Extract network properties
         self.latency_local = network_config["latency"]  # second
-        self.num_tiny = len(network_config["nodes"])
-        self.rams_tiny = []
+        self.num = len(network_config["nodes"])
+        self.rams = []  # available runtime memory
+        self.p_factors = []  # parallelization efficiency
+        self.rate_up = network_config["rate_up"]  # upload speed [GB/s]
+        self.rate_down = network_config["rate_down"]  # download speed [GB/s]
         for node in network_config["nodes"]:
-            self.rams_tiny.append(network_config["nodes"][node])  # GB
+            self.rams.append(network_config["nodes"][node]["ram"])  # GB
+            self.p_factors.append(network_config["nodes"][node]["p_factor"])  # parallelization efficacy factor
 
 
 def check_network_validity(num_workflows: int, num_tiny: int):
@@ -101,14 +93,18 @@ class CloudsInfo:
             cloud_config = json.load(f)
 
         # Extract cloud providers' information
-        self.names_cloud = list(cloud_config.keys())
-        self.num_clouds = len(self.names_cloud)
-        self.prices_cloud_transfer = []
-        self.prices_cloud_ram = []
-        self.prices_cloud_start = []
-        self.latency_cloud = []
+        self.names = list(cloud_config.keys())
+        self.num = len(self.names)
+        self.prices_transfer_up = []
+        self.prices_transfer_down = []
+        self.prices_ram = []
+        self.prices_start = []
+        self.latency = []
+        self.p_factors = []
         for provider in cloud_config.values():
-            self.prices_cloud_transfer.append(provider["pricing_Storage_Transfer"])  # $/GB
-            self.prices_cloud_ram.append(provider["pricing_RAM"])  # GB*s
-            self.prices_cloud_start.append(provider["pricing_StartRequest"])  # $/request
-            self.latency_cloud.append(provider["estimated_latency"])  # second
+            self.prices_transfer_up.append(provider["pricing_Storage_Transfer"]["upload"])  # $/GB
+            self.prices_transfer_down.append(provider["pricing_Storage_Transfer"]["download"])  # $/GB
+            self.prices_ram.append(provider["pricing_RAM"])  # GB*s
+            self.prices_start.append(provider["pricing_StartRequest"])  # $/request
+            self.latency.append(provider["estimated_latency"])  # second
+            self.p_factors.append(provider["p_factor"])  # parallelization efficacy factor
